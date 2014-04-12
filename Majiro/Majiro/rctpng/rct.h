d456b54773bad2efc6ec5cda0580340b297e8102
Fuyin <811197881@qq.com>
2014-04-13 00:22:42 +0800

    rct
    add some function

--- Majiro/Majiro/rctpng/rct.h
+++ Majiro/Majiro/rctpng/rct.h
@@ -3,11 +3,14 @@
 
 #include <Windows.h>
 #include <stdio.h>
+#include <string>
+
 
 #include "png.h"
 #include "zconf.h"
 #include "zconf.h"
 
+using namespace std;
 
 #pragma pack (1)
 
@@ -25,7 +28,20 @@ typedef struct {		/* 接下来0x300字节是调色版 */
 	DWORD data_length;
 } rc8_header_t;
 
+ typedef struct _pic_data
+{
+	unsigned int width, height; /* 尺寸 */
+	int bit_depth;  /* 位深 */
+	int flag;   /* 一个标志，表示是否有alpha通道 */
+
+	unsigned char *rgba; /* 图片数组 */
+ } pic_data;
+
 #pragma pack ()		
+
+#define PNG_BYTES_TO_CHECK 4
+#define HAVE_ALPHA 1
+#define NO_ALPHA 0
 
 
 class RCT
@@ -35,5 +51,9 @@ public:
 private:
 	DWORD rc8_decompress(BYTE *uncompr, DWORD uncomprLen, BYTE *compr, DWORD comprLen, DWORD width);
 	DWORD rct_decompress(BYTE *uncompr, DWORD uncomprLen, BYTE *compr, DWORD comprLen, DWORD width);
+	int dump_rc8(rc8_header_t *rc8, DWORD length, BYTE **ret_buf, DWORD *ret_len);
+	int dump_rct(rct_header_t *rct, DWORD length, BYTE **ret_buf, DWORD *ret_len);
+	int read_png_file(string filepath, pic_data *out);
+	int write_png_file(string file_name, pic_data *graph);
 };
 #endif
