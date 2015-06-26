32490eafda8edb875e56f780bbb783e8172c2665
Saya <wiki908.zh@gmail.com>
2015-06-27 00:11:36 +0800

    clean
    when you clone, arg '--depth=1' is recommended

--- crass(crage)/cui/System4/mt19937int.h
+++
@@ -1,7 +0,0 @@
-#ifndef MT19937INT_H
-#define MT19937INT_H
-	
-void sgenrand(unsigned long seed);
-unsigned long genrand(void);
-
-#endif	/* MT19937INT_H */
--- crass(crage)/cui/cmvs/cmvs_md5.h
+++
@@ -1,4 +0,0 @@
-typedef struct {
-	unsigned int buffer[16];
-	unsigned int state[4];
-} cmvs_md5_ctx;
