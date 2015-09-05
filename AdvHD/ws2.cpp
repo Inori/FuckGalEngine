b2f4988d3597b538e36aafdd2390617d61b0cbbf
aki <wiki908.zh@gmail.com>
2015-09-05 16:27:31 +0800

    update ws2
    fix bug: missing message at begin of script
    add specified game support

--- AdvHD/ws2.cpp
+++ AdvHD/ws2.cpp
@@ -7,6 +7,7 @@
 #include <string.h>
 
 #define PROC 2
+//#define CCR
 
 int wmain(int argc, wchar_t** argv)
 {
@@ -35,7 +36,7 @@ int wmain(int argc, wchar_t** argv)
 #if PROC == 1 // roll-right 2 bits
 
     unsigned char* ubuf = (unsigned char*)buf;
-    for (int i = 0; i < len; ++i)    
+    for (int i = 0; i < len; ++i)
         ubuf[i] = (ubuf[i] >> 2) | (ubuf[i] << 6);
     FILE* fo = _wfopen(argv[2], L"wb+");
     if (fo == 0) exit(1);
@@ -46,24 +47,39 @@ int wmain(int argc, wchar_t** argv)
     FILE* fo = _wfopen(argv[2], L"w+");
     if (fo == 0) exit(1);
     FILE* fa = _wfopen(argv[3], L"w+");
+    if (fa == 0) exit(1);
 
     int noChar = 1;
     int order = 1;
-    
+#ifdef CCR
+    int mkCCR = 0;
+#endif
+
     char CharStr[200];
     int CharOff = 0;
     char MainStr[800];
     int MainOff = 0;
 
-    for (char* anc = buf + 5; anc < buf + len; ++anc)
+    for (char* anc = buf; anc < buf + len; ++anc)
     {
-        if (memcmp(anc - 3, "%LC", 3) == 0 || memcmp(anc - 3, "%LF", 3) == 0)
+        if (anc - buf > 3 && memcmp(anc - 3, "%LC", 3) == 0 || memcmp(anc - 3, "%LF", 3) == 0)
         {
             strcpy(CharStr, anc);
             CharOff = anc - buf;
             noChar = 0;
         }
-        if (memcmp(anc - 5, "char\0", 5) == 0)
+#ifdef CCR
+        if (anc - buf > 15 && memcmp(anc - 15, "29\0CUTIN_32_001", 15) == 0)
+        {
+            mkCCR = 1;
+        }
+        if (anc - buf > 13 && memcmp(anc - 13, "7st29\0" "7st30\0" "e", 13) == 0)
+        {
+            //printf("-");
+            mkCCR = 0;
+        }
+#endif
+        if (anc - buf > 5 && memcmp(anc - 5, "char\0", 5) == 0)
         {
             strcpy(MainStr, anc);
             MainOff = anc - buf;
@@ -75,10 +91,13 @@ int wmain(int argc, wchar_t** argv)
             // line A
             fprintf(fo, "Åõ%04dÅõ", order);
             if (!noChar) fprintf(fo, "%sÅõ", CharStr);
-            fprintf(fo, "\n%s\n", MainStr);
+            fprintf(fo, " ##\n%s ##\n", MainStr);
             // line B
             fprintf(fo, "Åú%04dÅú", order);
             if (!noChar) fprintf(fo, "%sÅú", CharStr);
+#ifdef CCR
+            if (mkCCR) fprintf(fo, " < CCR >");
+#endif
             fprintf(fo, "\n%s\n", MainStr);
             // blank
             fprintf(fo, "\n");
@@ -87,7 +106,7 @@ int wmain(int argc, wchar_t** argv)
             fprintf(fa, "M %04d %08X %04X\n", order, MainOff, strlen(MainStr));
 
             noChar = ++order;
-        }        
+        }
     }
 
     fclose(fa);
@@ -96,6 +115,6 @@ int wmain(int argc, wchar_t** argv)
 
     fclose(fi);
     delete[] buf;
-	return 0;
+    return 0;
 }
 
