8fee337b053124732b6f130b9c99181c678af58e
Neroldy <yuanhong742604720@gmail.com>
2017-02-07 19:07:47 +0800

    Add new pattern.
    添加了一些新的pattern，重写了部分代码。测试游戏：oyakoai2 mamahaha

--- PJADV/text_in.py
+++ PJADV/text_in.py
@@ -1,58 +1,57 @@
 # -*- coding:utf-8 -*-
 
-import struct,os,fnmatch,re
+import struct, os, fnmatch, re
 
-#遍历文件夹，返回文件列表
+# 遍历文件夹，返回文件列表
 def walk(adr):
-    mylist=[]
-    for root,dirs,files in os.walk(adr):
+    mylist = []
+    for root, _, files in os.walk(adr):
         for name in files:
-            adrlist=os.path.join(root, name)
+            adrlist = os.path.join(root, name)
             mylist.append(adrlist)
     return mylist
 
-#将4字节byte转换成整数
+# 将4字节byte转换成整数
 def byte2int(byte):
-    long_tuple=struct.unpack('L',byte)
+    long_tuple = struct.unpack('L', byte)
     long = long_tuple[0]
     return long
 
-#提取txt中offset, name和string，转换为二维列表
+# 提取txt中offset, name和string，转换为二维列表
 def makestr(lines):
     sum_list = []
-    for index,line in enumerate(lines):
-        if '●' in line:
+    for _, line in enumerate(lines):
+        if '☆' in line:
             offset_str = re.search('([0-9A-Fa-f]+)', line).group()
             offset = int(offset_str, 16)
             continue
-        if '◎' in line:
-            if '★' in line:
-                name = line[10:-2]
-                continue
-            else:
-                name = ''
-                continue
-        if '◆' in line:
-            string = line[10:-2]
+        if '★' in line:
+            name = line[10:-1]
+            continue
+        if '●' in line:
+            string = line[10:-1]
             sum_list.append([offset, name, string])
             continue
-            
     return sum_list
 
+script_path = r'd:/archive/'
+# 注意这会覆写原来的文件，注意文件备份！
+index_path = script_path + 'scenario.dat'
+dst_path = script_path + 'textdata.bin'
 
-index = open('scenario.dat', 'rb+')
-dst = open('script.bin', 'rb+')
-dst.seek(0x20, 0)
+index = open(index_path, 'rb+')
+dst = open(dst_path, 'rb+')
+# jump header
+dst.seek(0x10, 0)
 
-namedic={}
-
-fl = walk('script')
+namedic = {}
+fl = walk(script_path)
 for fn in fl:
-    #过滤非txt文件
-    if not fnmatch.fnmatch(fn,'*.txt'):
+    # 过滤非txt文件
+    if not fnmatch.fnmatch(fn, '*.txt'):
         continue
     print(fn)
-    
+
     src = open(fn, 'r', encoding='utf-16')
     lines = src.readlines()
     sumlist = makestr(lines)
@@ -68,19 +67,19 @@ for fn in fl:
         else:
             nameoffset = 0
         stringoffset = dst.tell()
+#        print(cell[2])
         dst.write(cell[2].encode('gbk'))
         dst.write(struct.pack('H', 0))
-        
         index.seek(cell[0])
         long_byte = index.read(4)
         long = byte2int(long_byte)
-        if long == 0x80000307 or long == 0x80000406:        
+        if long == 0x80000307 or long == 0x80000406:
             index.seek(4, 1)
             index.write(struct.pack('L', nameoffset))
             index.write(struct.pack('L', stringoffset))
-        elif long == 0x01010203:
+        elif long == 0x01010203 or long == 0x81010102 or long == 0x01000D02 or long == 0x01010804:
             index.write(struct.pack('L', stringoffset))
-        elif long == 0x03000303:
+        elif long == 0x03000303 or long == 0x01003004 or long == 0x0404130C:
             index.seek(4, 1)
             index.write(struct.pack('L', stringoffset))
 
---
+++ PJADV/text_out.py
@@ -0,0 +1,126 @@
+# -*- coding:utf-8 -*-
+
+import struct
+
+# 将4字节byte转换成整数
+def byte2int(byte):
+    long_tuple = struct.unpack('L', byte)
+    long = long_tuple[0]
+    return long
+
+def dumpstring(file, offset):
+    file.seek(offset)
+
+    string = ''
+    bytestream = b''
+
+    for _ in range(0, 0xffffffff):
+        byte = file.read(1)
+        char_tuple = struct.unpack('B', byte)
+        char = char_tuple[0]
+        if char == 0:
+            byte = file.read(1)
+            char_tuple = struct.unpack('B', byte)
+            char2 = char_tuple[0]
+
+            if char2 == 0:
+                string += str(bytestream, encoding='sjis')
+                # string += str(b'\x81\x9a', encoding='sjis', errors='ignore')
+                break
+            else:
+                bytestream += struct.pack('B', char)
+                bytestream += struct.pack('B', char2)
+        else:
+            bytestream += struct.pack('B', char)
+    return string
+
+
+def scriptout(file, indexoffset, nameoffset, scriptoffset, count):
+    res = "☆%08x☆\n" % (indexoffset)
+
+    if nameoffset != 0:
+        name = dumpstring(file, nameoffset)
+        res += "★%08d★%s\n" % (count, name)
+    else:
+        name = ''
+        res += "★%08d★%s\n" % (count, name)
+
+    string = dumpstring(file, scriptoffset)
+    res += "○%08d○%s\n" % (count, string)
+    res += "●%08d●%s\n\n" % (count, string)
+    return res
+
+script_path = r'd:/archive/'
+index_path = script_path + 'scenario.dat'
+dst_path = script_path + 'textdata.bin'
+
+index = open(index_path, 'rb')
+src = open(dst_path, 'rb')
+
+src.seek(0, 2)
+startoffset = 0x20
+endoffset = src.tell()
+src.seek(0)
+
+indexoffsetlist = []
+scriptoffsetlist = []
+nameoffsetlist = []
+
+
+# 获取各地址存入相应列表
+index.seek(startoffset)
+for j in range(0, 0xffffffff):
+
+    long_byte = index.read(4)
+    long = byte2int(long_byte)
+    if long == 0x80000307 or long == 0x80000406:
+        indexoffsetlist.append(index.tell() - 4)
+
+        index.seek(index.tell() + 4)
+
+        nameoffset_byte = index.read(4)
+        nameoffset = byte2int(nameoffset_byte)
+        nameoffsetlist.append(nameoffset)
+
+        scriptoffset_byte = index.read(4)
+        scriptoffset = byte2int(scriptoffset_byte)
+        scriptoffsetlist.append(scriptoffset)
+    elif long == 0x01010203 or long == 0x81010102 or long == 0x01000D02 or long == 0x01010804:
+        indexoffsetlist.append(index.tell() - 4)
+
+        nameoffsetlist.append(0)
+
+        scriptoffset_byte = index.read(4)
+        scriptoffset = byte2int(scriptoffset_byte)
+        scriptoffsetlist.append(scriptoffset)
+    elif long == 0x03000303 or long == 0x01003004 or long == 0x0404130C:
+        indexoffsetlist.append(index.tell() - 4)
+        index.seek(index.tell() + 4)
+
+        nameoffsetlist.append(0)
+
+        scriptoffset_byte = index.read(4)
+        scriptoffset = byte2int(scriptoffset_byte)
+        scriptoffsetlist.append(scriptoffset)
+
+    else:
+        pass
+    if index.tell() >= endoffset:
+        break
+
+# 导出文本
+    size = len(indexoffsetlist)
+    fullname = script_path + 'script.txt'
+    dst = open(fullname, encoding='utf16', mode='w')
+    
+    for k in range(0, size):
+        string = scriptout(src, indexoffsetlist[k], nameoffsetlist[k], scriptoffsetlist[k], k)
+        dst.write(string)
+    dst.close()
+
+#     del indexoffsetlist[:]
+#     del scriptoffsetlist[:]
+#     del nameoffsetlist[:]
+
+index.close()
+src.close()
